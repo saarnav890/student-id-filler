9c7d8a7507c8b821afab591eb7385176a5ec1306
Aarnav <63823257+saarnav890@users.noreply.github.com>
2022-10-20 23:04:26 -0700

    about to submit

--- onboarding.js
+++ onboarding.js
@@ -6,8 +6,28 @@ function saveData() {
     chrome.storage.sync.set({id: document.getElementById("ida").value}, function() {
     document.getElementById('rect').remove()
     const header = document.getElementById("home")
-    const node = document.createTextNode("You're all set!")
-    header.appendChild(node)
+    const node = create()
+    const after = document.getElementById("after")
+    header.insertBefore(node, after)
     
 }) 
+}
+
+function link() {
+    window.location.href = "https://docs.google.com/forms/d/e/1FAIpQLSd_F4psuMJen_-MrD4oucfcE9eA8ZB3sMX-cp2JxRqiIJ8NXg/viewform?usp=sf_link";
+}
+
+function create() {
+    const allGood = document.createElement("div")
+    allGood.classList.add("goodText", "home-rectangle2")
+    allGood.textContent = "You're all set! Try it out below"
+
+    const but = document.createElement("div")
+    but.classList.add("butText", "home-rectangle4")
+    but.textContent = "Try"
+    allGood.appendChild(but)
+    but.addEventListener("click", link)
+
+    return allGood
+
 }
